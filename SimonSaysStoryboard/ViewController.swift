3b55c5d2d02918f1240595b1dcfb9980589bf073
Roman Yakovliev <r.yakovliev@zajno.com>
2021-10-06 00:08:27 +0300

    Initial Commit

---
+++ SimonSaysStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimonSaysStoryboard
+//
+//  Created by Roman Yakovliev on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
